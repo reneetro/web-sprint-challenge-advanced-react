76e96e41a1d7bb1f3e347dd5700f7a782c6492f5
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-04-14 20:52:11 -0600

    directions change coordinate state and reset button resets coordinates and message

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,8 +1,76 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 export default function AppFunctional(props) {
-
+  
   const [coordinates, setCoordinates] = useState([2,2]);
+  const [message, setMessage] = useState('');
+  const email = document.getElementById('email');
+
+  const handleDirection = (dir) => {
+    if(dir === 'up'){
+      if(coordinates[1] > 1){
+      setCoordinates([
+        coordinates[0], 
+        coordinates[1]-1
+      ])
+    }else{
+      setCoordinates([
+        ...coordinates
+      ])
+      setMessage(`You can't go up!`)
+    }
+  }
+    if(dir === 'down'){
+      if(coordinates[1] < 3){
+      setCoordinates([
+        coordinates[0], 
+        coordinates[1]+1
+      ])
+    }else{
+      setCoordinates([
+        ...coordinates
+      ])
+      setMessage(`You can't go down!`)
+    }
+  }
+    if(dir === 'left'){
+      if(coordinates[0] > 1){
+        console.log(coordinates)
+      setCoordinates([
+        coordinates[0]-1, 
+        coordinates[1]
+      ])
+    }else{
+      setCoordinates([
+        ...coordinates
+      ])
+      setMessage(`You can't go left!`)
+
+    }
+  }
+  if(dir === 'right'){
+    if(coordinates[0] < 3){
+    setCoordinates([
+      coordinates[0]+1, 
+      coordinates[1]
+    ])
+  }else{
+    setCoordinates([
+      ...coordinates
+    ])
+    setMessage(`You can't go right!`)
+  }
+}
+    return coordinates;
+}
+
+
+const handleReset = () => {
+  setCoordinates([2,2]);
+  setMessage('');
+}
+
 
 
 
@@ -13,25 +81,25 @@ export default function AppFunctional(props) {
         <h3 id="steps">You moved 0 times</h3>
       </div>
       <div id="grid">
+        <div className='square'></div>
         <div className="square"></div>
         <div className="square"></div>
         <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
+        <div className='square active'>B</div>
         <div className="square"></div>
         <div className="square"></div>
         <div className="square"></div>
         <div className="square"></div>
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={() => handleDirection('left')}>LEFT</button>
+        <button id="up" onClick={()=>handleDirection('up')}>UP</button>
+        <button id="right" onClick={()=>handleDirection('right')}>RIGHT</button>
+        <button id="down" onClick={()=>handleDirection('down')}>DOWN</button>
+        <button id="reset" onClick={handleReset}>reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
